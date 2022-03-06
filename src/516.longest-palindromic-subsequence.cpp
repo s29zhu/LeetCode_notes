e982a08bc318ca72866070777bdaf62b9b7b3707
Chen <wangchen412@gmail.com>
2022-03-05 17:38:54 -0700

    Optimizaiton of 516.

--- src/516.longest-palindromic-subsequence.cpp
+++ src/516.longest-palindromic-subsequence.cpp
@@ -10,26 +10,17 @@
 class Solution {
  public:
   int longestPalindromeSubseq(string s) {
-    int N = s.size(), len = 1;
-    array<int, 26> counts{0};
-    for (auto i : s) len = max(++counts[i - 'a'], len);
-
+    int N = s.size();
     vector<vector<int>> dp(N, vector<int>(N, 0));
     for (int i = 0; i < N; ++i) dp[i][i] = 1;
-    for (int i = 0; i < N - 1; ++i) {
-      dp[i][i + 1] = s[i] == s[i + 1] ? 2 : 1;
-      if (dp[i][i + 1] > len) len = dp[i][i + 1];
-    }
 
-    for (int i = 2; i < N; ++i)
-      for (int j = 0; j < N - i; ++j) {
-        dp[j][j + i] = max({dp[j + 1][j + i], dp[j][j + i - 1],
-                            dp[j + 1][j + i - 1] + (s[j] == s[j + i] ? 2 : 0)});
-        if (dp[j][j + i] > len) len = dp[j][j + i];
-      }
+    for (int i = 1; i < N; ++i)
+      for (int j = 0; j < N - i; ++j)
+        dp[j][j + i] = s[j] == s[j + i]
+                           ? dp[j + 1][j + i - 1] + 2
+                           : max(dp[j + 1][j + i], dp[j][j + i - 1]);
 
-    print_matrix(dp);
-    return len;
+    return dp.front().back();
   }
 };
 // @lc code=end
