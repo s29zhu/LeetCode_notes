d476d6530c27ed6d9c2d01a054bcf0a4b9829fe4
Chen <wangchen412@gmail.com>
2022-03-13 13:30:33 -0600

    Palindrom pair and stock iv.

--- src/188.best-time-to-buy-and-sell-stock-iv.cpp
+++ src/188.best-time-to-buy-and-sell-stock-iv.cpp
@@ -11,15 +11,26 @@ class Solution {
  public:
   int maxProfit(int k, vector<int>& prices) {
     int N = prices.size();
-    if (N == 1) return 0;
+    if (N < 2) return 0;
     int m = numeric_limits<int>::min() / 2;
-    vector<vector<int>> h(k, vector<int>(N, m)), s(k, vector<int>(N, m));
+    vector<vector<int>> h(N, vector<int>(k, m)), s(N, vector<int>(k, m));
     h[0][0] = -prices[0];
     for (int i = 1; i < N; ++i) {
-      for (int j = 0; j < k; ++j) {
-        
+      h[i][0] = max(h[i - 1][0], -prices[i]);
+      s[i][0] = max(s[i - 1][0], max(h[i - 1][0], h[i][0]) + prices[i]);
+      for (int j = 1; j < k; ++j) {
+        h[i][j] = max(h[i - 1][j], s[i - 1][j - 1] - prices[i]);
+        s[i][j] = max(s[i - 1][j], max(h[i - 1][j], h[i][j]) + prices[i]);
       }
     }
+    return *max_element(s.back().begin(), s.back().end());
   }
 };
 // @lc code=end
+
+int main() {
+  vector<int> prices{2, 1};
+  Solution s;
+  cout << s.maxProfit(1, prices) << endl;
+  return 0;
+}
---
+++ src/336.palindrome-pairs.cpp
@@ -0,0 +1,99 @@
+#include "stl_headers.hpp"
+
+/*
+ * @lc app=leetcode id=336 lang=cpp
+ *
+ * [336] Palindrome Pairs
+ */
+
+// @lc code=start
+
+class Solution {
+  struct Node {
+    char c{'.'};
+    int word_ID{-1};
+    vector<array<int, 2>> candidates;
+    unordered_map<char, Node*> s;
+    Node() {}
+    Node(char c) : c(c) {}
+    ~Node() {
+      for (auto& i : s) delete i.second;
+    }
+    Node* find(char c) { return s.find(c) == s.end() ? nullptr : s[c]; }
+  };
+  struct Dictionary {
+    Node* root;
+    Dictionary(const vector<string>& words) {
+      root = new Node();
+      for (int i = 0; i < words.size(); ++i) add_word(words[i], i);
+    }
+    ~Dictionary() { delete root; }
+    void add_word(const string& word, int ID) {
+      Node* r = root;
+      r->candidates.push_back({ID, -1});
+      int i = 0;
+      while (i < word.size()) {
+        Node* n = r->find(word[i]);
+        if (!n) break;
+        if (i < word.size() - 1) n->candidates.push_back({ID, i});
+        r = n;
+        ++i;
+      }
+      if (i != word.size())
+        add_new_word(r, word, ID, i);
+      else
+        r->word_ID = ID;
+    }
+    void add_new_word(Node* root, const string& word, int ID, int start) {
+      Node* r = root;
+      for (int i = start; i < word.size(); ++i) {
+        Node* n = new Node(word[i]);
+        if (i < word.size() - 1) n->candidates.push_back({ID, i});
+        r->s[word[i]] = n;
+        r = n;
+      }
+      r->word_ID = ID;
+    }
+  };
+  bool is_palindrom(const string& word, int start, int end) {
+    for (int i = start, j = end; i < j; ++i, --j)
+      if (word[i] != word[j]) return false;
+    return true;
+  }
+
+  void find_pair(const vector<string>& words, int ID, const Dictionary& dict,
+                 vector<vector<int>>& rst) {
+    Node* r = dict.root;
+    if (r->word_ID != -1 && is_palindrom(words[ID], 0, words[ID].size() - 1) &&
+        r->word_ID != ID)
+      rst.push_back({r->word_ID, ID});
+    for (int i = words[ID].size() - 1; i >= 0; --i) {
+      r = r->find(words[ID][i]);
+      if (!r) return;
+      if (r->word_ID != -1 && is_palindrom(words[ID], 0, i - 1) &&
+          r->word_ID != ID)
+        rst.push_back({r->word_ID, ID});
+    }
+    for (auto& i : r->candidates)
+      if (is_palindrom(words[i[0]], i[1] + 1, words[i[0]].size() - 1) &&
+          i[0] != ID)
+        rst.push_back({i[0], ID});
+  }
+
+ public:
+  vector<vector<int>> palindromePairs(vector<string>& words) {
+    Dictionary dict(words);
+    vector<vector<int>> rst;
+    for (int i = 0; i < words.size(); ++i) find_pair(words, i, dict, rst);
+    return rst;
+  }
+};
+
+// @lc code=end
+
+int main() {
+  vector<string> words{"bat", "tab", "cat", "aba", ""};
+  Solution s;
+  auto r = s.palindromePairs(words);
+  return 0;
+}
